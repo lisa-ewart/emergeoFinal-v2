b00ccec83b9ce64bc0c13d975ccfc880ae74f710
Richard Hill III <rhillz718@gmail.com>
2018-04-23 11:46:12 -0400

    adapted chat component able to send message. need to fix user paths

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,7 +3,7 @@ import {
 	View,
 	 Text,
 } from 'react-native';
-import Backend from './Backend';
+import firebase from 'firebase';
 import { GiftedChat } from 'react-native-gifted-chat';
 
 
@@ -11,22 +11,67 @@ class Chat extends React.Component {
 	state = {
 		messages: []
 	};
-	//before rendering of component do this
-	componentWillMount() {
 
+
+	setUid() {
+		this.props.screenProps.currentUser;
+	}
+
+	getUid() {
+		return this.props.screenProps.currentUser;
+	}
+
+
+
+    // retrieve the messages from the Backend
+
+	loadMessages(callback){
+		const uid = this.props.screenProps.currentUser;
+		this.messagesRef = firebase.database().ref('Users/'+ uid).child('messages');
+		this.messagesRef.off();
+		const onReceive = (data) => {
+			const message = data.val();
+			callback({
+				_id: data.key,
+				text:message.text,
+				createdAt: new Date(message.createdAt),
+				user: {
+					id: message.user.id,
+					name:message.user.name,
+				},
+			});
+		};
+		this.messagesRef.limitToLast(20).on('child_added', onReceive);
+	}
+
+	sendMessage(message) {
+		for (let i = 0; i < message.length; i++) {
+			this.messagesRef.push({
+				text:message[i].text,
+				user:message[i].user,
+				createdAt: firebase.database.ServerValue.TIMESTAMP,
+			});
+		}
+	}
+
+	closeChat() {
+		if (this.messagesRef) {
+			this.messagesRef.off();
+		}
 	}
 
 	render() {
         console.log(this.props);
+
 		return (
 			<GiftedChat 
 				messages={this.state.messages}
 				onSend={(message) => {
 					//send message to your backend
-					Backend.sendMessage(message);
+					this.sendMessage(message);
 				}}
 				user={{
-					_id: Backend.getUid(),
+					_id: this.getUid(),
 					name: this.props.name,
 				}}
 			/>
@@ -35,7 +80,7 @@ class Chat extends React.Component {
 	}
 
 	componentDidMount() {
-		Backend.loadMessages((message) => {
+		this.loadMessages((message) => {
 			this.setState((previousState) => {
 				return {
 					messages:GiftedChat.append(previousState.messages, message),
@@ -44,14 +89,14 @@ class Chat extends React.Component {
 		});
 	}
 
-	componenyWillUnmount() {
-		Backend.closeChat();
+	componentWillUnmount() {
+		this.closeChat();
 	}
 }
 
-// Chat.defaultProps = {
-// 	name: 'John',
-// };
+Chat.defaultProps = {
+	name: 'John',
+};
 
 
 
